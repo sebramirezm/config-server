c977618af4a241fe135ef5d00d46250124817e32
sebramirezm <s.ramirezm@icloud.com>
2022-06-22 23:49:54 -0400

    Config server initial commit

---
+++ src/test/java/com/people/workshop/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.workshop.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
